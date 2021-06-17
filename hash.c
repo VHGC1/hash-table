ce3e37fb30e70671ab8c34bbffbba25d76ad0dc1
vitor costa <vitorjjhg@hotmail.com>
2021-06-17 11:21:25 -0300

    menu-organization

--- hash.c
+++ hash.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <conio.h>
 #include <stdlib.h>
+#include<time.h>
 #include "hash.h"
 
 /*
@@ -61,7 +63,6 @@ void excluir_alemento(node* node){
 
 void imprimeHash(Hash tab){
   int  i = 0;
-  //int cont = 0;
   
   for(i = 0; i < tam; i++){
     if(tab[i] != NULL){
@@ -76,47 +77,139 @@ void imprimeHash(Hash tab){
  }
 }
 
-/*
-void menu(){
-  int op, num_tab;
+void criarArquivo(FILE* arquivo)
+{
+ arquivo = fopen("hash.txt", "r");
+ if(arquivo == NULL)
+ {
+  arquivo = fopen("hash.txt", "w");
+  fclose(arquivo);
+ }else
+ {
+  return;
+ }
+}
 
+
+void escreverArquivo(FILE* arquivo, int elemento){
+ arquivo = fopen("hash.txt", "a");
+ fprintf(arquivo,"%3d\n",elemento);
+ fclose(arquivo);
+}
+
+void numero_aleatorio(){
+  int num;
+  FILE* arquivo;
+  srand(time(NULL));
   
+  for(int i = 0; i < tam; i++){
+    double maior = pow(2,16), menor = pow(2,2);
+    double range = maior-(-menor);
+    double div = RAND_MAX / range;
+    num = menor + (rand() / div);
+    //escreverArquivo(arquivo, num);
+  }
+}
 
-  while(op != 5){
-    
-    printf("[ 1 ] Criar tabela\n");
-    printf("[ 2 ] Mostrar tabela\n");
-    printf("[ 3 ] Unir\n");
-    printf("[ 4 ] Verificar existencia\n");
-    printf("[ 5 ] Inserir elementos\n");
-    printf("[ 6 ] Excluir elementos\n");
-    printf("[ 7 ] Sair\n");
+int carregaArquivo(Hash tab)
+{
+ int elemento;
+ FILE* arquivo;
+ arquivo = fopen("hash.txt","r");
+ fseek(arquivo,0,SEEK_END);
+ if(ftell(arquivo) == 0)
+ {
+  return 0;
+ }
+ fseek(arquivo,0,SEEK_SET);
+ if(arquivo == NULL)
+ {
+  return 0;
+ }else
+ {
+  while(!feof(arquivo))
+  {
+   fscanf(arquivo,"%d",&elemento);
+   insereHash(tab,elemento);
+  }
+  system("cls");
+ }
+ fclose(arquivo);
+ return 1;
+}
 
-    printf("Digitar op:");
-    scanf("%d", &op);
+void imprimeColisao(Hash tab, int pos){
+ node* aux = tab[pos];
+ if(aux == NULL)
+ {
+  printf("Esta posicao esta vazia!");
+  return;
+ }else
+ {
+  if(aux != NULL)
+  {
+   printf("%3d",aux->chave);
+   while(aux->prox != NULL)
+   {
+    printf(" -> %d",aux->prox->chave);
+    aux = aux->prox;
+   }
+  }
+ }
+}
 
-    switch (op){
-      case 1:
-        //criar()
-        break;
-      case 2:
-        //mostrar(tabela1);
-        break;
-      case 3:
-        //unir();
-        break;
-      case 4:
-        //existe();
-        break;
-      case 5:
-        //inserir_elementos();
-        break;
-      case 6:
-        //excluir_elemento();
-        break;
-      case 7:
-        exit(1);  
+void removeHash(Hash tab, int num){
+ int pos = hash(num);
+ int ex ;
+ if(pos > tam){
+  printf("\nEsta posicao nao existe na tabela!");
+ }else{
+  if(tab[pos] == NULL){
+   printf("Esta chave esta vazia!");
+  }else{
+   printf("\n\n\n");
+   imprimeColisao(tab,pos);
+   printf("\n\nQual registro deseja apagar =  ");
+   scanf("%d",&ex);
+   if(tab[pos]->chave == ex){
+    if(tab[pos]->prox == NULL){
+     tab[pos] = NULL;
+     return;
+    }
+    if(tab[pos]->prox != NULL){
+     tab[pos]->chave = tab[pos]->prox->chave;
+     tab[pos]->prox = tab[pos]->prox->prox;
+     return;
+    }
+   }else{
+   if(tab[pos]->chave != ex){
+    if(tab[pos]->prox == NULL){
+     printf("\nRegistro nao encontrado!");
+     getch();
+     return;
+    }else{
+    node* ant = NULL;
+    node* aux = tab[pos]->prox;
+    while(aux->prox != NULL  && aux->chave != ex){
+     ant = aux;
+     aux = aux->prox;
+    }
+    if(aux->chave != ex){
+     printf("\nRegistro nao encontrado!\n");
+     return;
+    }else{
+      if(ant == NULL){
+        tab[pos]->prox = aux->prox;
+      }else{
+        ant->prox = aux->prox;
+      }
+      aux = NULL;
+      free(aux);
+            }
+          }
+        }
+      }
     }
   }
 }
-*/
+
--- hash_exe.c
+++ hash_exe.c
@@ -7,10 +7,11 @@ main(){
   Hash tab2;
   Hash tab3;
 
-  int op, num_tab = 0;
+  int op, num_tab = 0, num_del, num_ins;
 
+  //FILE* arquivo;
+  //criarArquivo(arquivo);
   
-
   while(op != 5){
     
     printf("No de tabelas %d\n", num_tab);
@@ -33,6 +34,7 @@ main(){
 
         if(num_tab == 1){
           inicializaHash(tab1);
+          
         }
 
         if (num_tab ==2){
@@ -46,29 +48,144 @@ main(){
           inicializaHash(tab3);
         }
         
-
-        //criar()
         break;
       case 2:
-        printf("[ 1 ] abrir csv");
-        printf("[ 2 ] imprimir");
-        imprimeHash(tab1);
+
+      printf("Mostrar tabela\n");
+      printf("[ 1 ] tabela 1\n");
+      printf("[ 2 ] tabela 2\n");
+      printf("[ 3 ] tabela 3\n");
+      
+      scanf("%d", &op);
+
+      if(op == 1){
+        printf("[ 1 ] imprimir\n");
+        printf("[ 2 ] abrir csv\n");
+
+        printf("Entre com a opcao desejada:\n");
+        scanf("%d", &op);
+
+        if(op == 1){
+          imprimeHash(tab1);
+        }
+
+        if(op == 2){
+
+        }
+      }
+      
+      if (op ==2){
+        printf("[ 1 ] imprimir\n");
+        printf("[ 2 ] abrir csv\n");
+
+        printf("Entre com a opcao desejada:\n");
+        scanf("%d", &op);
+
+        if(op == 1){
+          imprimeHash(tab2);
+        }
+
+        if(op == 2){
+
+        }
+      }
+
+      if(op == 3){
+        printf("[ 1 ] imprimir\n");
+        printf("[ 2 ] abrir csv\n");
+
+        printf("Entre com a opcao desejada:\n");
+        scanf("%d", &op); 
+
+        if(op == 1){
+          imprimeHash(tab3);
+        }
+
+        if(op == 2){
+
+        }
+      }
+        
         break;
       case 3:
-        for(int i = 0; i < tam; i++){
-            int a = rand() % 100;
-            inserir_elementos(tab1, a);
-          }
+        //Hash tab_merge;
         //unir();
         break;
       case 4:
         //existe();
         break;
       case 5:
-        //inserir_elementos();
+
+      printf("Remover valor\n");
+        printf("[ 1 ] tabela 1\n");
+        printf("[ 2 ] tabela 2\n");
+        printf("[ 3 ] tabela 3\n");
+      
+        scanf("%d", &op);
+
+        if(op == 1){
+          imprimeHash(tab1);
+
+          printf("Numero a ser inserido:\n");
+          scanf("%d", &num_ins);
+
+          inserir_elementos(tab1, num_ins);
+        }
+
+        if(op == 2){
+          imprimeHash(tab2);
+
+          printf("Numero a ser inserido:\n");
+          scanf("%d", &num_ins);
+
+          inserir_elementos(tab2, num_ins);
+        }
+
+        if(op == 3){
+          imprimeHash(tab3);
+
+          printf("Numero a ser inserido:\n");
+          scanf("%d", &num_ins);
+
+          inserir_elementos(tab3, num_ins);         
+        }
+
         break;
       case 6:
-        //excluir_elemento();
+        printf("Remover valor\n");
+        printf("[ 1 ] tabela 1\n");
+        printf("[ 2 ] tabela 2\n");
+        printf("[ 3 ] tabela 3\n");
+      
+        scanf("%d", &op);
+
+        if(op == 1){
+          imprimeHash(tab1);
+
+          printf("Numero a ser removido:\n");
+          scanf("%d", &num_del);
+
+          removeHash(tab1, num_del);
+        }
+
+        if(op == 2){
+          imprimeHash(tab2);
+
+          printf("Numero a ser removido:\n");
+          scanf("%d", &num_del);
+
+          removeHash(tab2, num_del);
+        }
+
+        if(op == 3){
+          imprimeHash(tab3);
+
+          printf("Numero a ser removido:\n");
+          scanf("%d", &num_del);
+          
+          removeHash(tab3, num_del);          
+        }
+        
         break;
       case 7:
         exit(1);  
